094732820f32ab42ed9af9e5dc532b3cb0130ca8
danila bogdan <danilabogdan2018@gmail.com>
2023-02-22 16:36:54 +0200

    Practice testing with Cypress

--- cypress/e2e/XPathLocators.cy.js
+++ cypress/e2e/XPathLocators.cy.js
@@ -4,5 +4,9 @@ describe('XPathLocators', () => {
     it('find no of products', () => {
         
         cy.visit("https://automationexercise.com/")
+
+        cy.xpath("//a[@href='/products']").click()
+
+        
     });
 });
